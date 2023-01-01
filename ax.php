6dbb125fb64eb36c90adf611a9a54f21e8200c25
santositb <119649954+santositb@users.noreply.github.com>
2023-01-02 00:13:28 +0700

    Update ax.php

--- ax.php
+++ ax.php
@@ -1,1 +1,16 @@
+<?php
 
+
+echo @color('purple', "NOMOR\t\t: ");
+$nomor = trim(fgets(STDIN));
+$login = login($nomor);
+echo @color('green', $login['message']."\n");
+
+
+
+function login($nomor){
+	$host = "bimaplus.tri.co.id";        
+    $data = '{"imei":"Android 93488a982824b403","language":1,"msisdn":"'.$nomor.'"}';
+    $ceknom = rekuest($host,"POST",'/api/v1/login/otp-request', $data);
+        return $ceknom;
+}
